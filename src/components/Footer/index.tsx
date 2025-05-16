16308827a9b723f883b4f604175a39dbb68eface
Andresa Alves <andresa_15ga@hotmail.com>
2025-05-16 18:13:16 -0300

    feat: finalizado componente

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import Image from 'next/image'
+
 export const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-black shadow-md z-50">
-      <div className="max-w-7xl mx-auto px-4 py-4 text-center">
-        <p className="text-sm text-gray-600 m-0">
-          &copy; {new Date().getFullYear()} Plan Frontend Test. All rights reserved.
+    <footer className="fixed bottom-0 w-full bg-black z-50">
+      <div className="max-w-7xl mx-auto px-4 py-4 text-center flex justify-between items-end">
+        <Image src="/img/LOGO GRUPO.svg" alt="Logo da Plan Marketing" width={126} height={92} />
+        <p className="text-sm text-white m-0">
+          Grupo Plan Marketing &copy; Todos os direitos reservados - {new Date().getFullYear()}
         </p>
       </div>
     </footer>
